6278f4b10674a0e7adc59bc7405e99f3e16f305e
Amir Negative <negative_amir@yahoo.com>
2016-02-28 17:49:34 +0330

    Update left_group.lua

--- plugins/left_group.lua
+++ plugins/left_group.lua
@@ -1,9 +1,9 @@
 do
 -- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = 182195776 -- your bot id
+local bot_id = 169430086 -- your bot id
    -- like local bot_id = 1234567
-    if matches[1] == 'leavenod32' and is_admin(msg) then
+    if matches[1] == 'leavetrain' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups fuck you (coming soon u Globall Banned and Blocked of this bot KOSE NANAT️).', ok_cb, false)
@@ -14,7 +14,7 @@ end
  
 return {
   patterns = {
-    "^[!/](leavenod32)$",
+    "^[!/](leavetrain)$",
     "^!!tgservice (.+)$",
   },
   run = run
