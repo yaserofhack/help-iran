ebd34184c97922412b19259970115f3782679258
Amir Negative <negative_amir@yahoo.com>
2016-02-28 17:45:17 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -6,11 +6,11 @@ do
  
  
    local text = matches[1]
-   local chat = "chat#id"..158990680 
+   local chat = "chat#id"..95702405 
    --like : local chat = "chat#id"..12345678
    
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'بازخورد شما ارسال شد'
+  return 'پیام شما ارسال شد👍'
  
  end
  end
